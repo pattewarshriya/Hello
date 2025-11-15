1e2a29cc60208fe9079988ae64b3f4d7f97feebe
pattewarshriya <68187213+pattewarshriya@users.noreply.github.com>
2025-11-15 11:57:30 +0530

    Create PrimeCheck.java

---
+++ PrimeCheck.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num =20;
+        
+        boolean isPrime = true;
+
+        // 0 and 1 are not prime numbers
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // Check divisibility from 2 to sqrt(num)
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a Prime Number.");
+        } else {
+            System.out.println(num + " is NOT a Prime Number.");
+        }
+
+        sc.close();
+    }
+}
